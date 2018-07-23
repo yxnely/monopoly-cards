e7be5dd44c74093e2edc73a3ad7c37e54846f611
Yanely <yanely_rmz@yahoo.com>
2018-07-22 22:34:26 -0400

    add: list vie + colors

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+const url = "https://raw.githubusercontent.com/daviscodesbugs/gamepiece-json/master/games/monopoly/monopoly.json"
+
+const cardVue = new Vue({
+	el: '#app',
+	data: {
+		cards: [],
+	},
+	created: function () {
+		this.getCards()
+	},
+	methods: {
+		getCards() {
+			const app = this;
+
+			fetch(url)
+				.then(function(res) {
+					return res.json();
+				})
+				.then(function(res) {
+					app.cards = res.pieces.properties[0].values;
+					console.log(app);
+				})
+		}
+	}
+});
